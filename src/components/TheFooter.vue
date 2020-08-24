64a687ca27e2b11bb9522aba1ca25b810805fb60
Nelson M <mwinel@live.com>
2020-08-24 17:27:47 +0300

    first commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer :class="$style.wrap">
+    <p>&copy; Awesome Company</p>
+  </footer>
+</template>
+
+<style lang="scss" module>
+.wrap {
+  padding-top: 1em;
+  padding-bottom: 1em;
+  border-top: 1px solid #c0c0c0;
+}
+</style>
